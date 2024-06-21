15b43ff26499c87d088581f58f025f232505275f
boguinho <160252917+boguinho@users.noreply.github.com>
2024-06-21 11:55:12 -0300

    Add files via upload

--- projeto_biblioteca.sql
+++ projeto_biblioteca.sql
@@ -7,3 +7,14 @@ create table professor(
     cpf varchar(45),
     pk int
 );
+
+create table sala(
+	pk int,
+    numero_sala varchar(4),
+    qtd_aluno int
+);
+
+create table turma(
+	pk int,
+    nome_turma varchar(45)
+);
